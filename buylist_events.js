1b9097f88f14461e3e086a505e81a1c972962c76
mykolayes <32031506+mykolayes@users.noreply.github.com>
2017-10-05 00:30:00 +0300

    0.9.1 js

--- buylist_events.js
+++ buylist_events.js
@@ -1,11 +1,13 @@
 $(function(){
-    alert("Hi!");
+    //alert("Hi!");
     var LIST = $('.bl-list');
     var ITEM_TEMPLATE = $('.bl-row').html();
 
     function addItem(title) {
         var node = $(ITEM_TEMPLATE); //Create new HTML node
-        node.find(".bl-product").text(title); //Set product title
+        //node.addClass("bl-row");
+        node.css("border-bottom-style", "solid");
+        node.find("bl-product").text(title); //Set product title
 //Delete Action
         node.find(".bl-remove-item").click(function(){
             node.remove();
@@ -13,8 +15,10 @@ $(function(){
         LIST.append(node); //Add to the end of the list
     }
     $(".bl-add").click(function(){
-        var name = document.getElementsByClassName("bl-text-add-field")[whole_number].value;
+        //var name = document.getElementsByClassName(".bl-text-add-field")[whole_number].value;
+        var name = document.getElementById('addingfield').value;
         addItem(name);
-        $(".bl-text-add-field").val("");
+        //$(".bl-text-add-field").val("");
+        $("#addingfield").val("");
     });
 });
