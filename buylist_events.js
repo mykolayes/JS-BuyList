e46b4b8fe0f27d3240af7e1d5522f6bf32b95634
mykolayes <32031506+mykolayes@users.noreply.github.com>
2017-10-07 13:11:59 +0300

    0.9.b

--- buylist_events.js
+++ buylist_events.js
@@ -1,22 +1,25 @@
 $(function(){
     //alert("Hi!");
-    var LIST = $('.bl-list');
-    var ITEM_TEMPLATE = $('.bl-row').html();
+    var $list = $(".bl-list");
+    //var $add_prod = $(".bl-add-product");
+    var ONE_ITEM_HTML = $(".one-row-template").html();
 
     function addItem(title) {
-        var node = $(ITEM_TEMPLATE); //Create new HTML node
-        //node.addClass("bl-row");
-        node.css("border-bottom-style", "solid");
-        node.find("bl-product").text(title); //Set product title
+        var $node = $(ONE_ITEM_HTML); //Create new HTML node
+        //$node.addClass("bl-row");
+        //$node.css("border-bottom-style", "solid");
+        $node.find(".bl-product").text(title); //Set product title
 //Delete Action
-        node.find(".bl-remove-item").click(function(){
-            node.remove();
+        $node.find(".bl-remove-item").click(function(){
+            $node.remove();
         });
-        LIST.append(node); //Add to the end of the list
+        $list.append($node); //Add to the end of the list
+        //$add_prod.append($node);
     }
     $(".bl-add").click(function(){
         //var name = document.getElementsByClassName(".bl-text-add-field")[whole_number].value;
-        var name = document.getElementById('addingfield').value;
+        //var name = document.getElementById('addingfield').val();
+        var name = $("#addingfield").val();
         addItem(name);
         //$(".bl-text-add-field").val("");
         $("#addingfield").val("");
